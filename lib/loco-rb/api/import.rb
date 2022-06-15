5d487ca084c60fd7c07a24f2156d970968959ca9
Andrew <andrew.goncharenko@ooma.com>
2022-06-15 13:39:56 -0700

    [#WOFFICE-15689] fix import

--- lib/loco-rb/api/import.rb
+++ lib/loco-rb/api/import.rb
@@ -6,8 +6,9 @@ module LocoRb
       def import_locale(options={ext: 'json', index: 'id'})
         opts = {}
         opts[:params] = { locale: options[:locale] } if options.dig( :locale)
-        response = post("#{PATH}/#{options[:ext]}", opts)
-        LocoRb::Resource::Locale.new(response.data, response.headers, 'locale').table
+        opts[:body] = IO.read(options[:path])
+        response = post("#{PATH}/#{options[:ext] || 'json'}", opts)
+        LocoRb::Resource::Import.new(response.data, response.headers, 'import')
       end
     end
   end
