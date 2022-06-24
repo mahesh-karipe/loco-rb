c1202d7bf4d612aa5cfb8d94c79dddbb43b94d02
Andrew <andrew.goncharenko@ooma.com>
2022-06-24 10:29:26 -0700

    [#WOFFICE-15689] add tag_updated support for import_locale

--- lib/loco-rb/api/import.rb
+++ lib/loco-rb/api/import.rb
@@ -3,9 +3,10 @@ module LocoRb
     module Import
       PATH = 'import'
 
-      def import_locale(options={ext: 'json', index: 'id'})
+      def import_locale(options={ext: 'json'})
         opts = {}
         opts[:params] = { locale: options[:locale] } if options.dig( :locale)
+        opts[:params] = opts[:params].merge({ "tag-updated": options[:tag_updated] }) if options.dig( :tag_updated)
         opts[:body] = IO.read(options[:path])
         response = post("#{PATH}/#{options[:ext] || 'json'}", opts)
         LocoRb::Resource::Import.new(response.data, response.headers, 'import')
